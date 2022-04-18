23928785f9027043528c6181f50af60dd37e4195
Dawson Israel <dawfre@amazon.com>
2022-04-18 10:06:23 -0300

    Configure CDK Stack to create cloudwatch query

--- dev/cdk/lib/sauron_stack.ts
+++ dev/cdk/lib/sauron_stack.ts
@@ -1,13 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { SauronLambda } from './lambda'
-import { SauronStateMachine } from './step_functions'
+import { CfnQueryDefinition } from 'aws-cdk-lib/aws-logs'
 
 export class SauronStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
         const sauronLambda = new SauronLambda(this, 'SauronLambda')
-        new SauronStateMachine(this, 'SauronStateMachine', sauronLambda.sauronLambdaHandler)
+        new CfnQueryDefinition(this, 'ListLogsQuery', {
+            name: 'ListLogs',
+            queryString: 'fields @timestamp, @message | sort @timestamp desc',
+            logGroupNames: [sauronLambda.sauronLambdaHandler.logGroup.logGroupName],
+        })
     }
 }
