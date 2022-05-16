8bf19827bc5b3b35cd31e2f207be0eff4efa01f3
Dawson Israel <dawfre@amazon.com>
2022-05-16 13:47:11 -0300

    Add Execute Query integration test

---
+++ tests/execute_query_test.rs
@@ -0,0 +1,15 @@
+use cw_sauron::LogClient;
+use chrono::Utc;
+
+#[tokio::test]
+async fn should_execute_query_and_return_valid_result() {
+    let log_client = LogClient::new().await;
+
+    let execution = log_client.execute_query(
+        "878917c1-b1a7-47ae-9177-2e11226b9323".to_string(),
+        Utc::now(),
+        Utc::now()
+    ).await;
+
+    assert!(execution.is_ok());
+}
